150641781998d07879ed141df4b980ebb8246aa5
vasiljeu95 <vasilyeu95@gmail.com>
2019-11-28 19:22:18 +0300

    Initial Commit

---
+++ Table View SwiftBook First App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View SwiftBook First App
+//
+//  Created by Stepan Vasilyeu on 11/28/19.
+//  Copyright © 2019 Stepan Vasilyeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
